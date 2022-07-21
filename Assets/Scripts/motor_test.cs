439268691b208316935a6a9a75e1510d1f3dff63
Takeshi Miura <57553950+1222-takeshi@users.noreply.github.com>
2022-07-22 00:09:01 +0900

    Feature/add one leg simulation (#6)
    * add sample script
    
    Signed-off-by: 1222-takeshi <m.takeshi1995@gmail.com>
    
    * add one leg simulation
    
    Signed-off-by: 1222-takeshi <m.takeshi1995@gmail.com>

---
+++ Assets/Scripts/motor_test.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class motor_test : MonoBehaviour
+{
+  HingeJoint joint;
+  JointMotor motor;
+  // Start is called before the first frame update
+  void Start()
+  {
+    joint = gameObject.GetComponent<HingeJoint>();
+    motor = joint.motor;
+    Debug.Log("start");
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKey(KeyCode.Space)) {
+      motor_on_front();
+    }
+    if (Input.GetKey(KeyCode.LeftControl)) {
+      motor_on_back();
+    }
+  }
+
+  void motor_on_front()
+  {
+    // motor.targetVelocity = 5;
+    // joint.useMotor = true;
+    motor.targetVelocity = 10;
+    joint.motor = motor;
+    Debug.Log(joint.gameObject.name);
+  }
+  void motor_on_back()
+  {
+    motor.targetVelocity = -10;
+    joint.motor = motor;
+    Debug.Log("motor on back");
+  }
+}
---
+++ Assets/Scripts/rotate_test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate_test : MonoBehaviour
+{
+
+  //
+  private GameObject Joint1;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    Joint1 = GameObject.Find("Link1Front1");
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+    float dx = Input.GetAxis("Horizontal") * Time.deltaTime * 3;
+    // float dz = Input.GetAxis("Vertical") * Time.deltaTime * 3;
+    // Link1Front.transform.Rotate(dx, 0, 0);
+    Joint1.transform.Rotate(2, 0, 0);
+  }
+}
