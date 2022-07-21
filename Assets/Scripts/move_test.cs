82c845bc1b81354f43b1ca5988a55a2c2c2a2c0f
1222-takeshi <m.takeshi1995@gmail.com>
2022-07-22 00:05:28 +0900

    add sample script
    Signed-off-by: 1222-takeshi <m.takeshi1995@gmail.com>

--- Assets/Scripts/move_test.cs
+++ Assets/Scripts/move_test.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class move_test : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+    float dy = Input.GetAxis("Horizontal") * Time.deltaTime * 3;
+    float dz = Input.GetAxis("Vertical") * Time.deltaTime * 3;
+    transform.position = new Vector3(
+      transform.position.x, transform.position.y + dy, transform.position.z + dz
+    );
+  }
 }
