5c45b524e234604eb47458d17d78a7e7ece38e46
1222-takeshi <m.takeshi1995@gmail.com>
2022-07-30 19:34:07 +0900

    add one leg walk simulation
    Signed-off-by: 1222-takeshi <m.takeshi1995@gmail.com>

--- Assets/Scripts/leg_controller.cs
+++ Assets/Scripts/leg_controller.cs
@@ -6,46 +6,48 @@ public class leg_controller : MonoBehaviour
 {
   HingeJoint joint;
   JointMotor motor;
-  bool motor_on_ = false;
   // Start is called before the first frame update
   void Start()
   {
     joint = gameObject.GetComponent<HingeJoint>();
     motor = joint.motor;
+    motor_on();
   }
 
   // Update is called once per frame
   void Update()
   {
-    motor_on();
     if (Input.GetKey(KeyCode.Space)) {
       bending_leg();
     } else if (Input.GetKey(KeyCode.LeftControl)) {
       extending_leg();
-    } else if (Input.GetKey(KeyCode.Return)) {
+    } else if (Input.GetKey(KeyCode.W)) {
+      modeWalk();
+    } else if (Input.GetKey(KeyCode.Backspace)) {
       motor_off();
     }
   }
 
   void motor_on()
   {
+    CancelInvoke();
     motor.targetVelocity = 100;
     motor.force = 45;
     joint.motor = motor;
     joint.useMotor = true;
     Debug.Log("motor on");
-    motor_on_ = true;
   }
 
   void motor_off()
   {
+    CancelInvoke();
     joint.useMotor = false;
     Debug.Log("motor off");
-    motor_on_ = false;
   }
 
   void bending_leg()
   {
+    CancelInvoke();
     motor.targetVelocity = -100;
     Debug.Log("bending leg");
     joint.motor = motor;
@@ -53,8 +55,124 @@ public class leg_controller : MonoBehaviour
 
   void extending_leg()
   {
+    CancelInvoke();
     motor.targetVelocity = 100;
     Debug.Log("extending leg");
     joint.motor = motor;
+  }
+
+  void modeWalk()
+  {
+    Debug.Log("mode walk");
+    InvokeRepeating("walkStep1", 0, 3);
+    InvokeRepeating("walkStep2", 1, 3);
+    InvokeRepeating("walkStep2", 2, 3);
+  }
+
+  void walkStep1()
+  {
+    if (gameObject.transform.parent.name == "FrontLeft") {
+      if (gameObject.transform.name == "Joint") {
+        motor.targetVelocity = -100;
+      } else if (gameObject.transform.name == "Link1Rear") {
+        motor.targetVelocity = -50;
+      }
+      joint.motor = motor;
+    } else if (gameObject.transform.parent.name == "RearRight") {
+      if (gameObject.transform.name == "Joint") {
+        motor.targetVelocity = -100;
+      } else if (gameObject.transform.name == "Link1Rear") {
+        motor.targetVelocity = -50;
+      }
+      joint.motor = motor;
+    }
+    if (gameObject.transform.parent.name == "FrontRight") {
+      if (gameObject.transform.name == "Joint") {
+        motor.targetVelocity = -50;
+      } else if (gameObject.transform.name == "Link1Rear") {
+        motor.targetVelocity = 200;
+      }
+      joint.motor = motor;
+    } else if (gameObject.transform.parent.name == "RearLeft") {
+      if (gameObject.transform.name == "Joint") {
+        motor.targetVelocity = -50;
+      } else if (gameObject.transform.name == "Link1Rear") {
+        motor.targetVelocity = 200;
+      }
+      joint.motor = motor;
+    }
+  }
+
+  void walkStep2()
+  {
+    if (gameObject.transform.parent.name == "FrontLeft") {
+      if (gameObject.transform.name == "Joint") {
+        motor.targetVelocity = 50;
+      } else if (gameObject.transform.name == "Link1Rear") {
+        motor.targetVelocity = -100;
+      }
+      joint.motor = motor;
+    } else if (gameObject.transform.parent.name == "RearRight") {
+      if (gameObject.transform.name == "Joint") {
+        motor.targetVelocity = 100;
+      } else if (gameObject.transform.name == "Link1Rear") {
+        motor.targetVelocity = 200;
+      }
+      joint.motor = motor;
+    }
+    if (gameObject.transform.parent.name == "FrontRight") {
+      if (gameObject.transform.name == "Joint") {
+        motor.targetVelocity = -100;
+      } else if (gameObject.transform.name == "Link1Rear") {
+        motor.targetVelocity = -50;
+      }
+      joint.motor = motor;
+    } else if (gameObject.transform.parent.name == "RearLeft") {
+      if (gameObject.transform.name == "Joint") {
+        motor.targetVelocity = -100;
+      } else if (gameObject.transform.name == "Link1Rear") {
+        motor.targetVelocity = -50;
+      }
+      joint.motor = motor;
+    }
+  }
+
+  void walkStep3()
+  {
+    if (gameObject.transform.parent.name == "FrontLeft") {
+      if (gameObject.transform.name == "Joint") {
+        motor.targetVelocity = 50;
+      } else if (gameObject.transform.name == "Link1Rear") {
+        motor.targetVelocity = 100;
+      }
+      joint.motor = motor;
+    } else if (gameObject.transform.parent.name == "RearRight") {
+      if (gameObject.transform.name == "Joint") {
+        motor.targetVelocity = 100;
+      } else if (gameObject.transform.name == "Link1Rear") {
+        motor.targetVelocity = 200;
+      }
+      joint.motor = motor;
+    }
+    if (gameObject.transform.parent.name == "FrontRight") {
+      if (gameObject.transform.name == "Joint") {
+        motor.targetVelocity = -100;
+      } else if (gameObject.transform.name == "Link1Rear") {
+        motor.targetVelocity = -50;
+      }
+      joint.motor = motor;
+    } else if (gameObject.transform.parent.name == "RearLeft") {
+      if (gameObject.transform.name == "Joint") {
+        motor.targetVelocity = -100;
+      } else if (gameObject.transform.name == "Link1Rear") {
+        motor.targetVelocity = -50;
+      }
+      joint.motor = motor;
+    }
+  }
+
+  void getParent()
+  {
+    Debug.Log("Parent name = " + gameObject.transform.parent.name);
   }
 }
---
+++ Assets/Scripts/leg_controller_one_leg.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leg_controller_one_leg : MonoBehaviour
+{
+  HingeJoint joint;
+  JointMotor motor;
+  JointLimits limits;
+  // Start is called before the first frame update
+  void Start()
+  {
+    joint = gameObject.GetComponent<HingeJoint>();
+    motor = joint.motor;
+    limits = joint.limits;
+    motor_on();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // getConnectedBody();
+    if (Input.GetKey(KeyCode.Space)) {
+      bending_leg();
+    } else if (Input.GetKey(KeyCode.LeftControl)) {
+      extending_leg();
+    } else if (Input.GetKey(KeyCode.W)) {
+      modeWalk();
+    } else if (Input.GetKey(KeyCode.Backspace)) {
+      motor_off();
+    }
+  }
+
+  void motor_on()
+  {
+    CancelInvoke();
+    motor.force = 45;
+    joint.motor = motor;
+    joint.useMotor = true;
+    Debug.Log("motor on");
+  }
+
+  void motor_off()
+  {
+    CancelInvoke();
+    joint.useMotor = false;
+    Debug.Log("motor off");
+  }
+
+  void bending_leg()
+  {
+    CancelInvoke();
+    if (gameObject.transform.name == "Link1Front") {
+      motor.targetVelocity = 100;
+    }
+    if (gameObject.transform.name == "Link1Rear") {
+      motor.targetVelocity = -100;
+    }
+    Debug.Log("bending leg");
+    joint.motor = motor;
+  }
+
+  void extending_leg()
+  {
+    CancelInvoke();
+    if (gameObject.transform.name == "Link1Front") {
+      motor.targetVelocity = -100;
+    } else if (gameObject.transform.name == "Link1Rear") {
+      motor.targetVelocity = 100;
+    }
+    Debug.Log("extending leg");
+    joint.motor = motor;
+  }
+
+  void modeWalk()
+  {
+    Debug.Log("mode walk");
+    joint.useLimits = true;
+    if (gameObject.transform.name == "Link1Front") {
+      limits.min = -60;
+      limits.max = 60;
+    } else if (gameObject.transform.name == "Link1Rear") {
+      limits.min = -45;
+      limits.max = 135;
+    }
+    joint.limits = limits;
+    Invoke("walkStep1", 0);
+    InvokeRepeating("walkStep2", 0.5f, 3);
+    InvokeRepeating("walkStep3", 1.5f, 3);
+    InvokeRepeating("walkStep4", 2.5f, 3);
+  }
+
+  void walkStep1()
+  {
+    if (gameObject.transform.name == "Link1Front") {
+      motor.targetVelocity = 100;
+    } else if (gameObject.transform.name == "Link1Rear") {
+      motor.targetVelocity = 50;
+    }
+    joint.motor = motor;
+  }
+
+  void walkStep2()
+  {
+    if (gameObject.transform.name == "Link1Front") {
+      motor.targetVelocity = -50;
+    } else if (gameObject.transform.name == "Link1Rear") {
+      motor.targetVelocity = -100;
+    }
+    joint.motor = motor;
+  }
+
+  void walkStep3()
+  {
+    if (gameObject.transform.name == "Link1Front") {
+      motor.targetVelocity = -100;
+    } else if (gameObject.transform.name == "Link1Rear") {
+      motor.targetVelocity = -40;
+    }
+    joint.motor = motor;
+  }
+
+  void walkStep4()
+  {
+    if (gameObject.transform.name == "Link1Front") {
+      motor.targetVelocity = 150;
+    } else if (gameObject.transform.name == "Link1Rear") {
+      motor.targetVelocity = 50;
+    }
+    joint.motor = motor;
+  }
+
+  void getConnectedBody()
+  {
+    Debug.Log("get connected body" + joint.connectedBody.name);
+  }
+}
